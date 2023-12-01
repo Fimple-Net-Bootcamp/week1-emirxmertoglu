1a90b5357eb15fd7337fef006ecd2b84c98c17dd
Emir Mertoğlu <emir.mertoglu.97@gmail.com>
2023-12-01 12:24:51 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace emir_mertoglu_homework_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
